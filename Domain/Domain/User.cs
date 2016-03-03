4f681e81ce6589bd183d4c387676fa6a0fb7103f
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-03-03 17:07:19 +0100

    Add base Domain classes

---
+++ Domain/Domain/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class User
+    {
+        private string username;
+        public string Username { get; set; }
+
+        private string password;
+        public String Password { get; set; }
+
+        private Address address;
+        public Address Address { get; set; }
+    }
+}
