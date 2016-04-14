4001b2d767c4c4c0600e702a5f43743ceee179d3
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2016-04-14 10:05:53 +0200

    v0.1 of wpf view

--- Database/Database/DapperConfiguration.cs
+++ Database/Database/DapperConfiguration.cs
@@ -11,32 +11,38 @@ namespace Database
 {
     public class DapperConfiguration
     {
-        private static bool isInitialized;
+        private static bool isInitialized = false;
 
         public static void Initialize()
         {
             if (!isInitialized)
             {
-                FluentMapper.Initialize(config =>
-                {
-                    config.AddMap(new AddressMapper());
-                    config.AddMap(new CartEntryMapper());
-                    config.AddMap(new CartMapper());
-                    config.AddMap(new CustomerMapper());
-                    config.AddMap(new CustomerNotificationMapper());
-                    config.AddMap(new DeliveryMapper());
-                    config.AddMap(new InvoiceMapper());
-                    config.AddMap(new OrderItemMapper());
-                    config.AddMap(new OrderMapper());
-                    config.AddMap(new ProductCategoryMapper());
-                    config.AddMap(new ProductMapper());
-                    config.AddMap(new ProductReviewMapper());
-                    config.AddMap(new StockMapper());
-                    config.AddMap(new SupplierMapper());
-                    config.AddMap(new UserMapper());
-                    config.AddMap(new WarehouseMapper());
-                    config.ForDommel();
-                });
+//                try
+//                {
+                    FluentMapper.Initialize(config =>
+                    {
+                        config.AddMap(new AddressMapper());
+                        config.AddMap(new CartEntryMapper());
+                        config.AddMap(new CartMapper());
+                        config.AddMap(new CustomerMapper());
+                        config.AddMap(new CustomerNotificationMapper());
+                        config.AddMap(new DeliveryMapper());
+                        config.AddMap(new InvoiceMapper());
+                        config.AddMap(new OrderItemMapper());
+                        config.AddMap(new OrderMapper());
+                        config.AddMap(new ProductCategoryMapper());
+                        config.AddMap(new ProductMapper());
+                        config.AddMap(new ProductReviewMapper());
+                        config.AddMap(new StockMapper());
+                        config.AddMap(new SupplierMapper());
+                        config.AddMap(new UserMapper());
+                        config.AddMap(new WarehouseMapper());
+                        config.ForDommel();
+                    });
+//                } catch (Exception ex)
+//                {
+//                    Console.WriteLine(ex);
+//                }
                 isInitialized = true;
             }
         }
---
+++ ManagerApplication/ManagerApplication/ManageProductsUI.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Database;
+using Database.Broker;
+using Domain;
+using Service;
+
+namespace ManagerApplication
+{
+    /// <summary>
+    /// Interaction logic for ManageProductsUI.xaml
+    /// </summary>
+    public partial class ManageProductsUI : UserControl
+    {
+        private bool initialized = false;
+
+        public ManageProductsUI()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize()
+        {
+            if (!initialized)
+            {
+                initialized = true;
+                IEnumerable<Product> products = new CrudService<Product>().GetAll();
+                ListProducts.ItemsSource = products;
+            }
+        }
+
+        private void ListProducts_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                ProductUi.SelectedProduct = ListProducts.SelectedItem as Product;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        
+    }
+}
